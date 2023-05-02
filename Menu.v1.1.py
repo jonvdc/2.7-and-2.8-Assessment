633dca97cbb4f6fac8cb9c1b3da990538140cc32
jonvdc <vandercolf2@middleton.school.nz>
2023-05-03 09:51:53 +1200

    Update Menu.v1.1.py

--- Menu.v1.1.py
+++ Menu.v1.1.py
@@ -1,25 +1,29 @@
 # Welcomes the user
 # Gives the user options
-print("Welcome!")
-choices = input("Choose an option from below: \n"
-                "1. Add Cards\n"
-                "2. Search for Cards\n"
-                "3. Delete Cards\n"
-                "4. Print All Cards \n"
-                "5. Exit\n\n"
-                "Enter choice: ")
 
-if choices == "1":
-    print("Add Cards action succeeded")
+# loop to make testing/trialling easier and
+# more efficient
+while True:
+    print("Welcome!")
+    choices = input("Choose an option from below: \n"
+                    "1. Add Cards\n"
+                    "2. Search for Cards\n"
+                    "3. Delete Cards\n"
+                    "4. Print All Cards \n"
+                    "5. Exit\n\n"
+                    "Enter choice: ")
 
-elif choices == "2":
-    print("Change Search for Cards action succeeded")
+    if choices == "1":
+        print("Add Cards action succeeded")
 
-elif choices == "3":
-    print("Delete Cards action succeeded")
+    elif choices == "2":
+        print("Change Search for Cards action succeeded")
 
-elif choices == "4":
-    print("Print All Cards action succeeded")
+    elif choices == "3":
+        print("Delete Cards action succeeded")
 
-elif choices == "5":
-    print()
+    elif choices == "4":
+        print("Print All Cards action succeeded")
+
+    elif choices == "5":
+        break
