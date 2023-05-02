4a4e64ed565cf7d11587a0981479a37259e70a72
jonvdc <vandercolf2@middleton.school.nz>
2023-05-03 10:28:32 +1200

    Finished first component

--- Menu.v1.2.py
+++ Menu.v1.2.py
@@ -1,4 +1,20 @@
+# Welcomes the user
+# Gives the user options
+
 import easygui
 
-def (menu):
-    
+
+def menu():
+    easygui.msgbox("Welcome to the Monster Card Storage Facility", "Welcome")
+
+    options = easygui.buttonbox("Choose an option from below: ", "Menu",
+                             choices=["Add Cards", "Search for Cards",
+                                      "Delete Cards", "Print All Cards",
+                                      "Exit"])
+
+    return options
+
+
+# Main routine
+choice = menu()
+easygui.msgbox(f"{choice} action succeeded")
