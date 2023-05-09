44c50031b31a6b7145454b08b77d267bfdd69e0a
jonvdc <vandercolf2@middleton.school.nz>
2023-05-10 10:03:17 +1200

    Renamed components

---
+++ 2_Storage.v1.0.py
@@ -0,0 +1,71 @@
+# Dictionary to store existing cards
+
+card_0 = {
+    "name": "Stoneling",
+    "strength": 7,
+    "speed": 1,
+    "stealth": 25,
+    "cunning": 15}
+
+card_1 = {
+    "name": "Vexscream",
+    "strength": 1,
+    "speed": 6,
+    "stealth": 21,
+    "cunning": 19}
+
+card_2 = {
+    "name": "Dawnmirage",
+    "strength": 5,
+    "speed": 15,
+    "stealth": 18,
+    "cunning": 22}
+
+card_3 = {
+    "name": "Blazegolem",
+    "strength": 15,
+    "speed": 20,
+    "stealth": 23,
+    "cunning": 6}
+
+card_4 = {
+    "name": "Websnake",
+    "strength": 7,
+    "speed": 15,
+    "stealth": 10,
+    "cunning": 5}
+
+card_5 = {
+    "name": "Moldvine",
+    "strength": 21,
+    "speed": 18,
+    "stealth": 14,
+    "cunning": 5}
+
+card_6 = {
+    "name": "Vortexwing",
+    "strength": 19,
+    "speed": 13,
+    "stealth": 19,
+    "cunning": 2}
+
+card_7 = {
+    "name": "Rotthing",
+    "strength": 16,
+    "speed": 7,
+    "stealth": 4,
+    "cunning": 12}
+
+card_8 = {
+    "name": "Froststep",
+    "strength": 14,
+    "speed": 14,
+    "stealth": 17,
+    "cunning": 4}
+
+card_9 = {
+    "name": "Wispghoul",
+    "strength": 17,
+    "speed": 19,
+    "stealth": 3,
+    "cunning": 2}
