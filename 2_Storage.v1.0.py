b3c86df012ab1d01a9905e80fc29377c0281c30e
jonvdc <vandercolf2@middleton.school.nz>
2023-05-16 12:01:16 +1200

    creation

--- 2_Storage.v1.0.py
+++ 2_Storage.v1.0.py
@@ -1,71 +1,64 @@
 # Dictionary to store existing cards
 
-card_0 = {
-    "name": "Stoneling",
-    "strength": 7,
-    "speed": 1,
-    "stealth": 25,
-    "cunning": 15}
-
-card_1 = {
-    "name": "Vexscream",
-    "strength": 1,
-    "speed": 6,
-    "stealth": 21,
-    "cunning": 19}
-
-card_2 = {
-    "name": "Dawnmirage",
-    "strength": 5,
-    "speed": 15,
-    "stealth": 18,
-    "cunning": 22}
-
-card_3 = {
-    "name": "Blazegolem",
-    "strength": 15,
-    "speed": 20,
-    "stealth": 23,
-    "cunning": 6}
-
-card_4 = {
-    "name": "Websnake",
-    "strength": 7,
-    "speed": 15,
-    "stealth": 10,
-    "cunning": 5}
-
-card_5 = {
-    "name": "Moldvine",
-    "strength": 21,
-    "speed": 18,
-    "stealth": 14,
-    "cunning": 5}
-
-card_6 = {
-    "name": "Vortexwing",
-    "strength": 19,
-    "speed": 13,
-    "stealth": 19,
-    "cunning": 2}
-
-card_7 = {
-    "name": "Rotthing",
-    "strength": 16,
-    "speed": 7,
-    "stealth": 4,
-    "cunning": 12}
-
-card_8 = {
-    "name": "Froststep",
-    "strength": 14,
-    "speed": 14,
-    "stealth": 17,
-    "cunning": 4}
-
-card_9 = {
-    "name": "Wispghoul",
-    "strength": 17,
-    "speed": 19,
-    "stealth": 3,
-    "cunning": 2}
+monster_cards = {
+    "Stoneling": {
+        "Strength": 7,
+        "Speed": 1,
+        "Stealth": 25,
+        "Cunning": 15,
+    },
+    "Vexscream": {
+        "Strength": 1,
+        "Speed": 6,
+        "Stealth": 21,
+        "Cunning": 19
+    },
+    "Dawnmirage": {
+        "Strength": 5,
+        "Speed": 15,
+        "Stealth": 18,
+        "Cunning": 22
+    },
+    "Blazegolem": {
+        "Strength": 15,
+        "Speed": 20,
+        "Stealth": 23,
+        "Cunning": 6,
+    },
+    "Websnake": {
+        "Strength": 7,
+        "Speed": 15,
+        "Stealth": 10,
+        "Cunning": 5,
+    },
+    "Moldvine": {
+        "Strength": 21,
+        "Speed": 18,
+        "Stealth": 14,
+        "Cunning": 5
+    },
+    "Vortexwing": {
+        "Strength": 19,
+        "Speed": 13,
+        "Stealth": 19,
+        "Cunning": 2
+    },
+    "Rotthing": {
+        "Strength": 16,
+        "Speed": 7,
+        "Stealth": 4,
+        "Cunning": 12
+    },
+    "Froststep": {
+        "Strength": 14,
+        "Speed": 14,
+        "Stealth": 17,
+        "Cunning": 4,
+    },
+    "Wispghoul": {
+        "Strength": 17,
+        "Speed": 19,
+        "Stealth": 3,
+        "Cunning": 2
+    }
+    }
